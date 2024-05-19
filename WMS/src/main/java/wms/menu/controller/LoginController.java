2d5b369aef09cbc115073f1f27b3a9b295beb46d
jeongeun1 <wjddms6188@gmail.com>
2024-05-19 22:30:45 +0900

    Feat: Login #15

--- WMS/src/main/java/wms/menu/controller/LoginController.java
+++ WMS/src/main/java/wms/menu/controller/LoginController.java
@@ -1,4 +1,16 @@
 package wms.menu.controller;
 
+import wms.menu.model.service.LoginService;
+
 public class LoginController {
+    private LoginService loginService = new LoginService();
+    public String checkId(String id) {
+        String result = loginService.checkId(id);
+        return result;
+    }
+
+    public String checkPw(String id) {
+        String result = loginService.checkPw(id);
+        return result;
+    }
 }
