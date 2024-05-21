0bc32b84e712baadf9736b478c5b3d1c3eeed028
jeongeun1 <wjddms6188@gmail.com>
2024-05-21 10:18:06 +0900

    Update: ResultView 정렬 수정

--- WMS/src/main/java/wms/Main.java
+++ WMS/src/main/java/wms/Main.java
@@ -1,6 +1,7 @@
 package wms;
 
 import org.apache.ibatis.session.SqlSession;
+import wms.menu.menuView.InOutboundView;
 import wms.menu.menuView.LoginView;
 
 import static wms.common.MyBatisTemplate.getSqlSession;
@@ -17,6 +18,7 @@ public class Main {
          * 4. resoureces 에서는 MyBatis.xml만 적용 했습니다
          * 5. JDBC, mybatis-config.xml 안에 주소를 확인해주시고 접속을 확인을 해주세요
          */
-        new LoginView().loginView();
+        // new LoginView().loginView();
+        new InOutboundView().inOutboundView();
     }
 }
